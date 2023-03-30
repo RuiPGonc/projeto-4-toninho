8e6a724e57e7f79dce98abd42e060b434247b335
Rui Pedro Gonçalves <ruipgonc@gmail.com>
2023-03-30 21:10:50 +0100

    up

--- Frontend/projeto-iv/src/components/generics/btnSubmit.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export default function SubmitButton({text,onClick}){
-    return(
-        <div>
-           <button onClick={onClick}>{text}</button>
-        </div>
-    )
-}
--- Frontend/projeto-iv/src/store/cleanStorage.jsx
+++
@@ -1,5 +0,0 @@
-export function cleanStorage(){
-
-localStorage.clear();
-sessionStorage.clear();
-}
